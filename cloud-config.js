80ca6625cd60ac0b3c3043db310bd07f2c8f3c80
WillieSP <seclenwj@gmail.com>
2026-08-08 09:38:34 -0500

    Delete cloud-config.js

--- cloud-config.js
+++
@@ -1,6 +0,0 @@
-// OPCIONAL: activa sincronización entre celulares/navegadores con Supabase.
-// Si dejas estos valores vacíos, ConstructNet funciona con almacenamiento local del navegador.
-window.CONSTRUCTNET_CLOUD = {
-  url: '',
-  anonKey: ''
-};
